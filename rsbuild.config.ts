50fb24b0208d829509a5452ae6e4561374a6f526
geha <ospac111@gmail.com>
2026-03-06 22:02:13 +0900

    feat: React Compiler 설치 및 설정

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/rspack'
+import { pluginBabel } from '@rsbuild/plugin-babel';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(),
+  pluginBabel({
+    include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions(opts) {
+        opts.plugins?.unshift('babel-plugin-react-compiler');
+      },
+  })
+  ],
   tools: {
     rspack: {
       plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true })],
